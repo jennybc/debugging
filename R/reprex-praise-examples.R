176fc177b7ad5638bc0ac7ccbdabf03a27356c79
Jenny Bryan <jenny.f.bryan@gmail.com>
2020-01-25 00:37:06 -0800

    Add praise + reprex examples

---
+++ R/reprex-praise-examples.R
@@ -0,0 +1,13 @@
+template <- "${EXCLAMATION} - your reprex is ${adjective}!"
+praise(template)
+
+#+ include = FALSE
+rm(template)
+
+#+ include = TRUE
+library(praise)
+praise(template)
+
+library(praise)
+template <- "${EXCLAMATION} - your reprex is ${adjective}!"
+praise(template)
